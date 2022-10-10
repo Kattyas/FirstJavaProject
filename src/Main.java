8a8cd745e0687dd5b324713a31b8bcecf1f4feb0
Catalin <katalinus_katty@yahoo.com>
2022-10-10 21:24:02 +0300

    Adoua

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Catalin!");
+        int x = 2;
+        int y = 5;
+
+        int z = sum(x, y);
+        System.out.println(z);
+
+        float a = substract(x, y);
+        System.out.println(a);
+
+        float unu = multiply(x, y);
+        System.out.println(unu);
+
+        float doi = divide(y, x);
+        System.out.println(doi);
+    }
+
+    public static int sum(int first, int second) {
+        int result = first + second;
+        return result;
+    }
+
+    public static float substract(float first, float second){
+        float result = first - second;
+        return result;
+    }
+    public static float multiply(float first, float second ){
+        float unu = first * second ;
+        return unu;
+    }
+    public static float divide(float first, float second){
+        float doi = first / second ;
+        return doi;
     }
 }
