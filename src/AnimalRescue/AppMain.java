358fb603b38e30dfcf371cdb09531b2370d19995
Catalin <katalinus_katty@yahoo.com>
2022-10-22 20:56:19 +0300

    TemaLab11

--- src/AnimalRescue/AppMain.java
+++ src/AnimalRescue/AppMain.java
@@ -12,14 +12,17 @@ public class AppMain {
         System.out.println("The dog's name is: " + rudolph.name);
         System.out.println("The dog's age is: "+ rudolph.age + " years ");
         System.out.println("The dog's weight is: " + rudolph.weight + " kg ");
-        System.out.println(yamYamFood.stock + " kg");
+        System.out.println("We have only " + yamYamFood.stock + " kg " + " of " + yamYamFood.name);
+        System.out.println("Rudolph like "+ jumping.name);
+        System.out.println("The name of the favorite food is " + yamYamFood.name);
 
         rudolph.eat();
         rudolph.sleep();
         rudolph.speak();
         rudolph.run();
         angelica.adopt();
-        mrWilly.ask();
+        mrWilly.consult();
+
 
 
 
--- src/AnimalRescue/RecAct.java
+++ src/AnimalRescue/RecAct.java
@@ -2,6 +2,7 @@ package AnimalRescue;
 
 public class RecAct {
     String name   = "jumping";
+    String name2  = "running";
 
     public void high(){
         System.out.println(" higher and higher ");
